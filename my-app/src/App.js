800a57007ec5ed9372b0e847652fae54bf35b30b
nicole930405 <nico0988736855@gmail.com>
2024-11-20 00:30:19 +0800

    轉跳頁面

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from "react";
+import Home from "./Home"
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";//轉跳頁面
+
 import Login_Signin from "./LoginModal/Login_Signin";
-import Enter_Address from "./search_address/Enter_Address";
 import Function_List from "./LoginModal/Function_List";
+import Modify_Data from "./Modify_Data/Modify_Data";
+import History_Order from "./History_Order/History_Order";
+
+
 
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
@@ -11,11 +18,30 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
+
 function App() {
 
     const [IsLogIn, setIsLogIn] = useState(false); //預設登入狀態(可自己調T、F)
     const [LoginModalOpen, setLoginModalOpen] = useState(false); //登入頁面
     const [FunctionList, setFunctionList] = useState(false) //以登入的使用者功能
+    const [user, setUser] = useState({
+        user_id: '',
+        user_name: '',
+    }); //user資料
+
+    //user假設
+    const [fake_user, setFakeUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/user/1")
+            .then(response => {
+                console.log("fetch data:", response.data);
+                setFakeUsers(response.data);
+            })
+            .catch(error => {
+                console.error("error fetching")
+            });
+    }, []);
     //const navigate = useNavigate(); //轉跳頁便用
 
     const [lng, setLng] = useState(null); //經度
@@ -74,74 +100,77 @@ function App() {
 
 
   return (
-    <div className="background">
-      <div className="top_bar">
-          <div className="component_container">
-                  <IconButton className="person_icon">
-                    <PersonOutlineOutlinedIcon onClick={handleIconClick_person}  style={{color:"black"}}/>
+      <Router>
+        <div className="background">
+          <div className="top_bar">
+              <div className="component_container">
+                      <IconButton className="person_icon">
+                        <PersonOutlineOutlinedIcon onClick={handleIconClick_person}  style={{color:"black"}}/>
+                      </IconButton>
+                  <div className="food_panda_color">
+                      foodpanda
+                  </div>
+                  {!IsLogIn ? (
+                      <>
+                          <Button variant="outlined"
+                                  sx={{
+                                      borderColor:'black',
+                                      color:'black'
+                                  }} className="login_button_login" onClick={handleIconClick_person}>
+                              登入
+                          </Button>
+                          <Button variant="contained"
+                                  sx={{
+                                      backgroundColor:'#e04c7f'
+                                  }} className="login_button_signup" onClick={handleIconClick_person}>
+                              註冊
+                          </Button>
+
+                      </>
+                  ):(
+                      <>
+                          <Button variant="contained"
+                                  sx={{
+                                      backgroundColor:'white',
+                                      color:'black',
+                                      boxShadow:'none',
+                                      '&:hover':{
+                                          boxShadow:'none'
+                                      }
+                                  }}
+                                  className="personal_icon"
+                                  startIcon={<PersonOutlineOutlinedIcon/>}
+                                  endIcon={<KeyboardArrowDownIcon
+                                      sx={{color:'#e04c7f'}}/>}
+                                  onClick={handleIconClick_person}
+                          >
+                              <div key={fake_user.user_id}>
+                                  {fake_user.user_name}
+                              </div>
+                          </Button>
+                      </>
+                  )}
+                  {/*未登入時顯示登入的按鈕 登入時顯示功能表*/}
+
+                  <IconButton className="shopping_car_icon">
+                      <ShoppingCartOutlinedIcon style={{color:"black"}} onClick={handleIconClick_shopping_cart}/>
                   </IconButton>
-              <div className="food_panda_color">
-                  foodpanda
+
+
               </div>
-              {!IsLogIn ? (
-                  <>
-                      <Button variant="outlined"
-                              sx={{
-                                  borderColor:'black',
-                                  color:'black'
-                              }} className="login_button_login" onClick={handleIconClick_person}>
-                          登入
-                      </Button>
-                      <Button variant="contained"
-                              sx={{
-                                  backgroundColor:'#e04c7f'
-                              }} className="login_button_signup" onClick={handleIconClick_person}>
-                          註冊
-                      </Button>
-
-                  </>
-              ):(
-                  <>
-                      <Button variant="contained"
-                              sx={{
-                                  backgroundColor:'white',
-                                  color:'black',
-                                  boxShadow:'none',
-                                  '&:hover':{
-                                      boxShadow:'none'
-                                  }
-                              }}
-                              className="personal_icon"
-                              startIcon={<PersonOutlineOutlinedIcon/>}
-                              endIcon={<KeyboardArrowDownIcon
-                                  sx={{color:'#e04c7f'}}/>}
-                              onClick={handleIconClick_person}
-                      >
-                          user
-                      </Button>
-                  </>
-              )}
-              {/*未登入時顯示登入的按鈕 登入時顯示功能表*/}
-
-              <IconButton className="shopping_car_icon">
-                  <ShoppingCartOutlinedIcon style={{color:"black"}} onClick={handleIconClick_shopping_cart}/>
-              </IconButton>
-
-
           </div>
-      </div> {/*最上面那一條*/}
-        <div className="move_text">
-            <div>從美食到生鮮雜貨 上千萬種商品</div>
-            <div>馬上點馬上到</div>
-        </div>
-        {/*未登入*/}
-        <Login_Signin isOpen={LoginModalOpen} onClose={closeLoginModal} origin_state={IsLogIn} now_stage={handle_setIsLogIn}/>
-        <Enter_Address  getLngLat = {handleLngLat} getAddress={setAddress}/>
-        <Function_List isOpen = {FunctionList} onClose ={closeFunctionList} />
-        {/*{console.log(lng)}*/}
-        {/*{console.log(address)}*/}
+            <Login_Signin isOpen={LoginModalOpen} onClose={closeLoginModal} origin_state={IsLogIn} now_stage={handle_setIsLogIn} getUser={setUser}/>
+            <Function_List isOpen = {FunctionList} onClose ={closeFunctionList} />{/*最上面那一條*/}
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/modify-data" element={<Modify_Data/>}/>
+                <Route path="/history-order" element={<History_Order/>}/>
+            </Routes>
 
-    </div>
+
+
+        </div>
+      </Router>
   );
 }
 
